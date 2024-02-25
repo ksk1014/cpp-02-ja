0ee837333b4e96e4ba7913388f87f69c7bbe9cec
keisuke-suzuki <keisuke.suzuki@woven-planet.global>
2024-02-25 17:55:19 +0900

    solve: 01_01_challenge

--- 01_objects/01_challenge/main.cpp
+++ 01_objects/01_challenge/main.cpp
@@ -1,1 +1,28 @@
 // コードを入力してください
+#include "traffic_light.h"
+#include <iostream>
+#include <cassert>
+
+int main(){
+
+    void test_main();
+    test_main();
+
+    TrafficLight light{TrafficLight::red};
+    std::string msg{};
+
+    msg = get_action(light);
+    
+    // std::cout << light << std::endl;
+    std::cout << msg << std::endl;
+
+};
+
+void test_main(){
+
+    assert(get_action(TrafficLight::red) == "Stop");
+    assert(get_action(TrafficLight::yellow) == "Attention");
+    assert(get_action(TrafficLight::green) == "Go");
+
+}
+
--- 01_objects/01_challenge/traffic_light.cpp
+++ 01_objects/01_challenge/traffic_light.cpp
@@ -1,1 +1,19 @@
 // コードを入力してください
+#include <string>
+#include "traffic_light.h"
+
+
+std::string get_action(TrafficLight light) {
+
+    std::string msg {};
+
+    if(light == TrafficLight::red){
+        msg = "Stop";
+    }else if(light == TrafficLight::yellow){
+        msg = "Attention";
+    }else if(light == TrafficLight::green){
+        msg = "Go";
+    };
+    return msg;
+    // コードを入力してください
+}
