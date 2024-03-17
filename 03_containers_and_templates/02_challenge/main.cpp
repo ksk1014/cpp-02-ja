051110d5796faf2613e11e7c75357c91cfa12f4c
keisuke-suzuki <keisuke.suzuki@woven-planet.global>
2024-03-17 16:54:12 +0900

    solved 03_02_challenge

--- 03_containers_and_templates/02_challenge/main.cpp
+++ 03_containers_and_templates/02_challenge/main.cpp
@@ -1,1 +1,24 @@
 // コードを入力してください
+#include "stack.hpp"
+#include <vector>
+#include <list>
+#include <deque>
+#include <iostream>
+
+int main(){
+
+    Stack<int> stack_int;
+    int a = 1;
+    std::cout << stack_int.is_empty() << std::endl;
+    stack_int.push(a);
+    std::cout << stack_int.is_empty() << std::endl;
+    stack_int.pop();
+    std::cout << stack_int.is_empty() << std::endl;
+
+    stack_int.push(2);
+    std::cout << stack_int.top() << std::endl;
+
+    stack_int.push(3);
+    std::cout << stack_int.top() << std::endl;
+
+}
--- 03_containers_and_templates/02_challenge/stack.hpp
+++ 03_containers_and_templates/02_challenge/stack.hpp
@@ -1,1 +1,28 @@
 // コードを入力してください
+
+#include <vector>
+#include <list>
+#include <deque>
+
+template <typename T>
+class Stack{
+    public:
+        Stack(): stack(){};
+
+        void push(const T& value){
+            stack.push_back(value);
+        };
+        void pop(){
+            if(!stack.empty()){
+            stack.pop_back();
+            };
+        };
+        T top(){
+            return stack.back();
+        };
+        bool is_empty() const{
+            return stack.empty();
+        };
+    private:
+        std::vector<T> stack;
+};
