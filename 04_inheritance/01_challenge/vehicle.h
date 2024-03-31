86950aea5e6ade821b5d62ddb099dd5a01abadba
keisuke-suzuki <keisuke.suzuki@woven-planet.global>
2024-03-31 18:13:21 +0900

    solved 04_01 challenge

---
+++ 04_inheritance/01_challenge/vehicle.h
@@ -0,0 +1,20 @@
+
+
+class Vehicle {
+    public:
+    private:
+        double velocity;
+        double fuel;
+    
+};
+
+class Car : public Vehicle {
+    private:
+        int passengerNum;
+};
+
+class Motorcycle : public Vehicle {
+    private:
+        int passengerNum;
+};
+
