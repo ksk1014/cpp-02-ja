d6f789021ed4a3bb481b242fd7a0b5807e779d30
keisuke-suzuki <keisuke.suzuki@woven-planet.global>
2024-03-31 18:36:44 +0900

    solced 04_02 challenge

--- 04_inheritance/02_challenge/circle.hpp
+++ 04_inheritance/02_challenge/circle.hpp
@@ -1,1 +1,12 @@
 // コードを入力してください
+
+#include "shape.hpp"
+
+class Circle : public Shape {
+public:
+    Circle(double radius) : m_radius(radius) {}
+    double getArea() { return 3.14159 * m_radius * m_radius; }
+
+private:
+    double m_radius;
+};
--- 04_inheritance/02_challenge/shape.hpp
+++ 04_inheritance/02_challenge/shape.hpp
@@ -1,1 +1,5 @@
 // コードを入力してください
+class Shape {
+public:
+    virtual double getArea() = 0;
+};
